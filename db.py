ef5c9b9a898c297f5bb09562f9e83e6165a4cbf4
juraqulovazizbek <juraqulovazizbek85@gmail.com>
2025-11-20 20:47:04 +0500

    db

--- db.py
+++ db.py
@@ -15,8 +15,14 @@ with psycopg2.connect(
     cursor.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username VARCHAR(150) NOT NULL UNIQUE , password VARCHAR(150))")
     # cursor.execute("INSERT INTO users (username, password) VALUES ('ALI', '5445') , ('Vali', '8787'); ")
 
+    name = input("username: ")
+    password = input("password: ")
+
+    cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (name, password))
+
     cursor.execute("SELECT * FROM users;")
     users = cursor.fetchall()
+    
     for user in users:
         print(user[0], user[1], user[2])
 
