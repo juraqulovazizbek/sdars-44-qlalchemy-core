8484969577b7ddd1d107aafe2d81abed860abefd
juraqulovazizbek <juraqulovazizbek85@gmail.com>
2025-11-20 20:26:29 +0500

    connection

--- config.py
+++ config.py
@@ -1,28 +1,13 @@
-import os
+import os 
 from dotenv import load_dotenv
-from sqlalchemy import (
-    create_engine, URL, MetaData,
-)
-
 load_dotenv()
-
 
 class Config:
     DB_USER = os.getenv('DB_USER')
-    DB_PASS = os.getenv('DB_PASS')
+    DB_PASS = os.getenv('DB_PASSWORD')
     DB_NAME = os.getenv('DB_NAME')
     DB_HOST = os.getenv('DB_HOST')
     DB_PORT = os.getenv('DB_PORT')
 
-config = Config()
 
-url = URL.create(
-    drivername='postgresql+psycopg2',
-    username=config.DB_USER,
-    password=config.DB_PASS,
-    host=config.DB_HOST,
-    port=config.DB_PORT,
-    database=config.DB_NAME
-)
-engine = create_engine(url)
-metadata_obj = MetaData()
+config = Config()
--- db.py
+++ db.py
@@ -3,24 +3,20 @@ from config import config
 
 
 with psycopg2.connect(
-    database=config.DB_NAME,
-    user=config.DB_USER,
-    password=config.DB_PASS,
-    host=config.DB_HOST,
-    port=config.DB_PORT
+    database = config.DB_NAME,
+    user = config.DB_USER,
+    password = config.DB_PASS,
+    host = config.DB_HOST,
+    port = config.DB_PORT
 ) as connection:
 
     cursor = connection.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username VARCHAR(127) NOT NULL UNIQUE, password VARCHAR(255));")
-    
-    name = input() 
-    password = input() 
-    cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s);", (name, password))
-    
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username VARCHAR(150) NOT NULL UNIQUE , password VARCHAR(150))")
+    # cursor.execute("INSERT INTO users (username, password) VALUES ('ALI', '5445') , ('Vali', '8787'); ")
+
     cursor.execute("SELECT * FROM users;")
     users = cursor.fetchall()
-    
     for user in users:
         print(user[0], user[1], user[2])
 
